2e868d7ef8033cd2ba2a3fd05aca57f9a34e4e61
dangvannhang <dangvannhang107@gmail.com>
2021-08-08 00:27:16 +0700

    Create GithubContext to contain githubuser, repos, followers

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from 'react'
+import { Info, Repos, User, Search, Navbar } from '../components'
+import loadingImage from '../images/preloader.gif'
+import { GithubContext } from '../context/context'
 const Dashboard = () => {
   return (
     <main>
       <h2>Dashboard Page</h2>
+      <Info></Info>
+      <Repos></Repos>
+      <User></User>
+      <Search></Search>
+      <Navbar></Navbar>
     </main>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
